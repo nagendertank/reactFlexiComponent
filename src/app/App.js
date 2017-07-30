32598dcaae49e9acd17cb08de313d19b450be38f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2017-07-31 00:49:38 +0530

    updated packages

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>React App</h2>
+            </div>
+        )
+    }
+}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,24 +5,21 @@ var APP_DIR = path.resolve(__dirname, 'src');
 
 var BUILD_DIR = path.resolve(__dirname, 'dist');
 
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 var config = {
-  
   entry: {
     app: APP_DIR + '/Main.js' 
   }, 
-
-
+   
   output: {
+    path: BUILD_DIR,
+    filename: 'bundle.[chunkhash].js',
     publicPath: '/'
   },
-   
-  // output: {
-  //   path: BUILD_DIR,
-  //   filename: 'bundle.js'
-  // },
  
 
   module : {
@@ -30,7 +27,7 @@ var config = {
       {
         test : /\.js?/,
         include : APP_DIR,
-        loaders: [ "babel-loader"]
+        loaders: ["babel-loader"]
       },
  
     ]
@@ -39,15 +36,39 @@ var config = {
   //debug, es6 to es5 mapping
   devtool: 'source-map',
 
+
   plugins: [
+
+     new CopyWebpackPlugin([
+      {
+        from: "src/assets",
+        to: "assets"
+      },
+
+      // {
+      //   from: "index.html",
+      //   to: "index.html"
+      // }
+    ]),
+    
     new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor',
-        filename: 'vendor.bundle.js',
+        filename: 'vendor.bundle.[chunkhash].js',
         minChunks:  function(module, count) {
             var context = module.context;
             return context && context.indexOf('node_modules') >= 0;
         },
     }),
+
+
+
+     new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true, // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+          warnings: false,
+        },
+      }),
+
 
       new HtmlWebpackPlugin({
         title: 'My Product App',
@@ -55,18 +76,9 @@ var config = {
         template: './src/index.html' //input file
       })
 
+
     
-  ],
-
-  devServer: {
-    contentBase: APP_DIR,
-    compress: true,
-    port: 8080,
-
-    historyApiFallback: {
-      index: 'index.html'
-    }
-  }
+  ]
 
 };
 
